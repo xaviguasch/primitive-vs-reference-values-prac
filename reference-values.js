05287629f13739e4a4eaee19443cfa6e8905f972
xaviguasch <xaviguasch@gmail.com>
2020-04-22 19:33:57 +0200

    added reference value equality prac

---
+++ reference-values.js
@@ -0,0 +1,11 @@
+const hobbies = ['Sports', 'Cooking']
+// const todos = ['Sports', 'Cooking'];
+const todos = hobbies
+
+const user = { age: 31 }
+const student = { age: 31 }
+
+console.log(hobbies == todos) // 5 == '5' => true
+console.log(hobbies === todos) // 5 === '5' => false
+console.log(user == student)
+console.log(user === student)
